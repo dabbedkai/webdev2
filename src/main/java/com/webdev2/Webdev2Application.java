eea9f69c8232e67c72ed6819e0d87377abaa52e6
dabbedkai <kaizerdavid.palabay@lorma.edu>
2026-09-10 13:11:08 +0800

    Lab 4

--- src/main/java/com/webdev2/Webdev2Application.java
+++ src/main/java/com/webdev2/Webdev2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Webdev2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Webdev2Application.class, args);
     }
